25a3bd881051ed98f12748891c9ef8c9fb9931c4
akillian@outlook.com <akillian@outlook.com>
2018-03-16 20:43:33 -0400

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
     const request = require('request');
     const apiKey = '8d4e75892426850a05baa1102dfaf243';
-    const city = 'portland';
+    const city = 'eastchester';
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-    console.log(url);
 
     request(url, function(err, response, body) {
         if(err) {
